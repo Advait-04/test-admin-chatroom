532cd657975a595a40c0da62fefdf9ebca98faae
advait <advait@advait-kali>
2023-08-01 19:18:06 +0530

    add encryption for messages sent

--- backend/controllers/chatroomController.js
+++ backend/controllers/chatroomController.js
@@ -1,12 +1,31 @@
+require("dotenv").config();
+
 const Chatroom = require("../models/chatroomModel");
+const crypto = require("crypto-js");
+
+const encryptText = (message) => {
+    const text = crypto.AES.encrypt(
+        message,
+        process.env.ENCRYPTION_KEY
+    ).toString();
+
+    return text;
+};
+
+const decryptText = (message) => {
+    const bytes = crypto.AES.decrypt(message, process.env.ENCRYPTION_KEY);
+    const text = bytes.toString(crypto.enc.Utf8);
+
+    return text;
+};
 
 const sendChat = async (req, res) => {
     const { room, user, text } = req.body;
 
-    console.log(room, user, text);
+    const encryptedText = encryptText(text);
 
     try {
-        const chat = await Chatroom.sendChat(room, user, text);
+        const chat = await Chatroom.sendChat(room, user, encryptedText);
         res.status(200).send(chat);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -18,6 +37,13 @@ const retrieveChat = async (req, res) => {
 
     try {
         const chatHistory = await Chatroom.retrieveChat(room);
+
+        chatHistory.chat.forEach((chat, index) => {
+            chatHistory.chat[index].text = decryptText(
+                chatHistory.chat[index].text
+            );
+        });
+
         res.status(200).json({ data: chatHistory });
     } catch (error) {
         res.status(400).send({ error: error.message });
