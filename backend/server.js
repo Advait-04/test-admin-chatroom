2bc194995acbceaca05fad861d0d855fe1d4fa03
advait <advait@advait-kali>
2023-08-01 20:02:07 +0530

    updates for hosting

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const userRoutes = require("./routes/user");
 const chatRoutes = require("./routes/chatroom");
 
 const mongoose = require("mongoose");
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
 
 // logger
 app.use((req, res, next) => {
@@ -20,11 +29,7 @@ app.use("/", userRoutes);
 app.use("/", chatRoutes);
 
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        // useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(5000, () => {
             console.log("connected to db and on port 5000");
@@ -33,3 +38,8 @@ mongoose
     .catch((error) => {
         console.log(error);
     });
+
+// mongodb://0.0.0.0:27017/mern
+// {useNewUrlParser: true,
+//         // useFindAndModify: false,
+//         useUnifiedTopology: true,}
