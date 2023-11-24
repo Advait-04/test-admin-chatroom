a79bc51ae69efab8c902ebd19a65b7142b8e695d
advait <advait@advait-kali>
2023-11-24 12:48:18 +0530

    fix front end signup not adding to concurrent users

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -38,10 +38,40 @@ export const useSignup = () => {
             //save the user to local storage
             localStorage.setItem("user", JSON.stringify(json));
 
+            //start the timer for logging the hours used by a user
+            localStorage.setItem(
+                "logs",
+                JSON.stringify({
+                    loginTime: Math.floor(Date.now() / 1000),
+                    roomTime: {},
+                    roomsAccessed: [],
+                    totalChats: 0,
+                })
+            );
+
             //update the auth context
             dispatch({ type: "LOGIN", payload: json });
 
-            setIsLoading(false);
+            //setting concurrent user
+            const concResponse = await fetch(
+                "https://mern-chatroom-backend.vercel.app/api/admin/addconcurrentuser",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ user: email }),
+                }
+            );
+
+            const concJson = await concResponse.json();
+
+            if (!concResponse.ok) {
+                setError(concJson.error);
+                setIsLoading(false);
+            }
+
+            if (concResponse.ok) {
+                setIsLoading(false);
+            }
         }
     };
 
